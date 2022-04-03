1f9b862a9c463604aaf5f12a7904a4ba567cf3ce
109598047 <t109598047@ntut.org.tw>
2022-04-03 10:21:48 +0800

    Build project.

---
+++ src/main/java/ntut/edu/tw/bookerfly/BookerflyApplication.java
@@ -0,0 +1,13 @@
+package ntut.edu.tw.bookerfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookerflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookerflyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ntut/edu/tw/bookerfly/BookerflyApplicationTests.java
@@ -0,0 +1,16 @@
+package ntut.edu.tw.bookerfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class BookerflyApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(1, 1);
+	}
+
+}
